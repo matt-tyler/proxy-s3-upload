acce431f62f4d0ffdd731eac66c1a1e3fc9a3b88
Matt Tyler <matt.tyler@mechanicalrock.io>
2020-07-06 10:31:22 +0800

    Fix linting

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,23 @@
-import * as core from '@actions/core'
-import {S3Proxy} from './s3'
+import * as core from "@actions/core";
+import { S3Proxy } from "./s3";
+
+async function run(): Promise<void> {
+    try {
+        const endpoint = core.getInput("endpoint", { required: true });
+        const bucket = core.getInput("bucket", { required: true });
+        const key = core.getInput("key", { required: true });
+        const filename = core.getInput("filename", { required: true });
+
+        const proxy = new S3Proxy(endpoint);
+        await proxy.UploadFile(bucket, key, filename);
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
 
 /* istanbul ignore next */
 if (require.main === module) {
-  run()
+    run();
 }
 
-module.exports = run
-
-async function run(): Promise<void> {
-  try {
-    const endpoint = core.getInput('endpoint', {required: true})
-    const bucket = core.getInput('bucket', {required: true})
-    const key = core.getInput('key', {required: true})
-    const filename = core.getInput('filename', {required: true})
-
-    const proxy = new S3Proxy(endpoint)
-    await proxy.UploadFile(bucket, key, filename)
-  } catch (error) {
-    core.setFailed(error.message)
-  }
-}
+module.exports = run;
